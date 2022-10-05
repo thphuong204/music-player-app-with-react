32d0daedcbdc82d27e6c79bede2a5de6752618d3
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-05 18:44:08 +0700

    add @TrackList component

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -1,18 +1,38 @@
 import React from "react";
 import useMusicPlayer from "../hooks/usePlayerProvider";
-
+import '../App.css';
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 
 const TrackList = () => {
-    const { trackList, currentTrackName, playTrack, isPlaying } =
+    const { trackList, currentTrackIndex, playTrack, isPlaying } =
         useMusicPlayer();
+        console.log("currentTrackIndex",currentTrackIndex, "isPlaying:",isPlaying);
     return (
-        <>
+        <div className="tracks-list">
             {trackList.map((track, index) => (
                 // ( Surprise us with your code here)
+                <div 
+                className="song-title-group"
+                key={index}
+                onClick={()=> {
+                    playTrack(index);
+                }}
+                >
+                    <div className="song-title">
+                        <div className="listen-icon" style={{fontSize:"10px"}}>
+                            {currentTrackIndex === index && isPlaying===true ? (
+                                <HeadphonesIcon/>
+                            )  : (
+                                <PauseCircleIcon/>
 
-                <div className="song-title">{track.name}</div>
+                            )}
+                        </div>
+                        <div className="song">{track.name}</div>
+                    </div>
+                </div>
             ))}
-        </>
+        </div>
     );
 };
 
